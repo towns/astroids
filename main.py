0f2bd5c0adfca4028e5c1307cf59613ded7c426c
towns <johntownsend@gmail.com>
2025-07-09 20:37:44 -0700

    added more functionality.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting Asteroids!")
@@ -7,13 +11,52 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updateable, drawable)
+    Shot.containers = (shots, updateable, drawable)
+    AsteroidField.containers = updateable
+    Player.containers = (updateable, drawable)
+    
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
-        screen.fill(0)
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
+           
+        updateable.update(dt)
+
+        # Check for collisions between Astroids and shots
+        # if there are any, kill both objects
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.split()
+                    shot.kill()
+
+        # Check for collisions between Astroids and Player
+        # If there is on, end the game
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over")
+                return
+        
+        screen.fill("black")
+
+        for item in drawable:
+            item.draw(screen)
+        
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
